bad8b69fee2cf876ea198aed9026a447af6db27e
biancasozzi <biancasozzi@gmail.com>
2018-09-03 16:09:56 -0300

    WIP

---
+++ Assets/CustomVirtualButtonEventHandler.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class CustomVirtualButtonEventHandler : MonoBehaviour,
+                                               IVirtualButtonEventHandler
+{
+    public GameObject panel;
+    void Start()
+    {
+
+        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vbs.Length; ++i)
+        {
+            vbs[i].RegisterEventHandler(this);
+        }
+
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("ACTUVE!");
+        panel.SetActive(true);
+
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        StartCoroutine(HidePanel());
+    }
+
+    public IEnumerator HidePanel()
+    {
+        Debug.Log("bye!");
+        yield return new WaitForSeconds(4);
+
+        panel.SetActive(false);
+
+    }
+}
