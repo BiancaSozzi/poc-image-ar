c12f24b77b27ac821aaf5fcaa2e7eff294b61077
biancasozzi <biancasozzi@gmail.com>
2018-09-04 12:05:27 -0300

    all targets with buttons

---
+++ Assets/ToggleButtonScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleButtonScript : MonoBehaviour {
+
+    public Button toggleButton;
+    public Text buttonText;
+    public GameObject panel;
+    public string playerName;
+
+    // Use this for initialization
+	void Start () {
+        toggleButton.GetComponent<Button>();
+        toggleButton.onClick.AddListener(TaskOnClick);
+        panel.SetActive(false);
+        SetButtonText();
+        
+	}
+	
+    void TaskOnClick(){
+      
+        panel.SetActive(!panel.activeSelf);
+        SetButtonText();
+    }
+
+    void SetButtonText(){
+
+        string status = "STATS";
+        if (panel.activeSelf)
+        {
+            status = "CHAMP";
+        }
+
+        buttonText.text = string.Format("{0} {1}", playerName, status);
+    }
+}
