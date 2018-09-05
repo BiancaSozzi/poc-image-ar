af5ed4e65d67bb40bb3755608fe9e28584cf0923
biancasozzi <biancasozzi@gmail.com>
2018-09-05 11:28:25 -0300

    Game presentation

---
+++ Assets/buttonScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonScript : MonoBehaviour {
+
+    public void openTwitch(){
+        Application.OpenURL("https://www.twitch.tv/faceittv");
+    }
+    public void openYouTube()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=fIVsRnP8Z-U");
+    }
+
+}
