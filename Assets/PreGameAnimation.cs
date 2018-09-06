fe496ceb9e53a9ae9752876b281d03c2f618256c
francob <betombino@gmail.com>
2018-09-06 12:29:55 -0300

    pre game scene

---
+++ Assets/PreGameAnimation.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PreGameAnimation : DefaultTrackableEventHandler{
+
+    public Text timer_text;
+    //private float time_remaining = 10; // in seconds
+
+    public GameObject panel_timer;
+    public GameObject panel_begin_game;
+
+    [Header("UI Animation")]
+    private RectTransform window_timer;
+    private RectTransform window_begin_game;
+    private Vector3 originalPosition_timer;
+    private Vector3 originalPosition_begin_game;
+    private float animationDistance;
+
+
+
+    void Awake()
+    {
+        animationDistance = Screen.height * 2f;
+        window_timer = panel_timer.GetComponent<RectTransform>();
+        window_begin_game = panel_begin_game.GetComponent<RectTransform>();
+        originalPosition_timer = window_timer.anchoredPosition3D;
+        originalPosition_begin_game = window_begin_game.anchoredPosition3D;
+    }
+
+    void Update()
+    {
+        if (timer_text.text.Contains("00:00:00"))
+        {
+            StartCoroutine(HidePanelTimer(this.window_timer));
+        }
+
+    }
+
+    protected override void OnTrackingFound()
+    {
+
+        base.OnTrackingFound();
+
+        window_timer.anchoredPosition3D += new Vector3(0f, -animationDistance, 0f);
+        window_begin_game.anchoredPosition3D += new Vector3(0f, -animationDistance, 0f);
+
+        LeanTween
+            .moveY(this.window_timer, this.window_timer.anchoredPosition3D.y + this.animationDistance, 3f)
+            .setEase(LeanTweenType.easeInOutSine);
+
+    }
+
+    public IEnumerator HidePanelTimer(RectTransform window)
+    {
+
+        LeanTween
+            .moveY(window, window.anchoredPosition3D.y - this.animationDistance, 3f)
+            .setEase(LeanTweenType.easeOutSine);
+
+        yield return new WaitForSeconds(0);
+
+        LeanTween
+            .moveY(this.window_begin_game, this.window_begin_game.anchoredPosition3D.y + this.animationDistance, 3f)
+            .setEase(LeanTweenType.easeInOutSine);
+
+        StartCoroutine(HidePanelGameBegins(this.window_begin_game));
+
+
+
+    }
+
+    public IEnumerator HidePanelGameBegins(RectTransform window)
+    {
+        yield return new WaitForSeconds(10);
+
+        LeanTween
+            .moveY(window, window.anchoredPosition3D.y - this.animationDistance, 3f)
+            .setEase(LeanTweenType.easeOutSine);
+
+        yield return new WaitForSeconds(3.5f);
+        SceneManager.LoadScene("match-presentation", LoadSceneMode.Single);
+
+    }
+
+}
---
+++ Assets/TimerPreMatch.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TimerPreMatch : MonoBehaviour {
+
+    public Text timer_text;
+    private float start_time;
+    private float time_remaining = 10; // in seconds
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time_remaining -= Time.deltaTime;
+
+        if(time_remaining <= 0){
+            timer_text.text = "MATCH BEGINS";
+        }else{
+
+
+            string seconds = (time_remaining % 60).ToString("f0");
+            if ((time_remaining % 60) < 10)
+            {
+                seconds = "0" + seconds;
+            }else if(seconds == "60"){
+                seconds = "00";
+            }
+
+
+            string minutes = ((int)(time_remaining / 60)).ToString();
+            if (((time_remaining / 60)) < 10)
+            {
+                minutes = "0" + minutes;
+            }
+
+
+            string hours = ((int)time_remaining / 3600).ToString();
+            if ((time_remaining / 3600) < 10)
+            {
+                hours = "0" + hours;
+            }
+
+            timer_text.text = hours + ':' + minutes + ':' +  seconds;
+            }
+
+	}
+
+}
