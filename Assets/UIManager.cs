e342bdbaec99a0adfa185aea18a5524f2a58fe81
biancasozzi <biancasozzi@gmail.com>
2018-09-06 09:28:11 -0300

    WIP

---
+++ Assets/UIManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    public Button[] playerButtons;
+    public GameObject[] playerPanels;
+    public GameObject buttonsPanel;
+ 
+    // Use this for initialization
+    void Start () {
+        playerButtons[0].onClick.AddListener(OpenPanel1);
+        playerButtons[1].onClick.AddListener(OpenPanel2);
+        playerButtons[2].onClick.AddListener(OpenPanel3);
+        HidePanels();
+    }
+
+    public void OpenPanel1()
+    {
+        Debug.Log("asdasd");
+        buttonsPanel.SetActive(false);
+        playerPanels[0].SetActive(true);
+
+    }
+
+    public void OpenPanel2()
+    {
+      
+    }
+
+    public void OpenPanel3()
+    {
+
+    }
+
+    public void HidePanels(){
+        for (int i = 0; i < playerPanels.Length; i++)
+        {
+            playerPanels[i].SetActive(false);
+        }
+        buttonsPanel.SetActive(true);
+    }
+
+
+}
